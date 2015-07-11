c84307c3bce8d97970b1688e7246f4e4999f4fa2
中西汐音 <s.ide@s-arcana.co.jp>
2015-07-11 15:46:19 +0900

    極ラーヴァナ追加

--- js/index.js
+++ js/index.js
@@ -492,6 +492,98 @@ var data = [
 		name: '討伐・討滅戦',
 		ids: [
 				{
+				name: '極ラーヴァナ',
+				items: [
+					{
+						name: 'ラーヴァナシャムシール',
+						shortName: '片手剣',
+						job: 0,
+						icon: 'images/item/goku_ravana/a01.png'
+					},
+					{
+						name: 'ラーヴァナスクトゥム',
+						shortName: '盾',
+						job: 0,
+						icon: 'images/item/goku_ravana/b01.png'
+					},
+					{
+						name: 'ラーヴァナバトルアクス',
+						shortName: '斧',
+						job: 0,
+						icon: 'images/item/goku_ravana/a02.png'
+					},
+					{
+						name: 'ラーヴァナクレイモア',
+						shortName: '両手剣',
+						job: 0,
+						icon: 'images/item/goku_ravana/a03.png'
+					},
+					{
+						name: 'ラーヴァナクロー',
+						shortName: '拳',
+						job: 0,
+						icon: 'images/item/goku_ravana/a04.png'
+					},
+					{
+						name: 'ラーヴァナスピア',
+						shortName: '槍',
+						job: 0,
+						icon: 'images/item/goku_ravana/a06.png'
+					},
+					{
+						name: 'ラーヴァナクリス',
+						shortName: '双剣',
+						job: 0,
+						icon: 'images/item/goku_ravana/a05.png'
+					},
+					{
+						name: 'ラーヴァナボウ',
+						shortName: '弓',
+						job: 0,
+						icon: 'images/item/goku_ravana/a07.png'
+					},
+					{
+						name: 'ラーヴァナマスケトン',
+						shortName: '銃',
+						job: 0,
+						icon: 'images/item/goku_ravana/a08.png'
+					},
+					{
+						name: 'ラーヴァナロングポール',
+						shortName: '黒杖',
+						job: 0,
+						icon: 'images/item/goku_ravana/a09.png'
+					},
+
+					{
+						name: 'ラーヴァナグリモア',
+						shortName: '召本',
+						job: 0,
+						icon: 'images/item/goku_ravana/a10.png'
+					},
+
+					{
+						name: 'ラーヴァナケーン',
+						shortName: '白杖',
+						job: 0,
+						icon: 'images/item/goku_ravana/a11.png'
+					},
+					{
+						name: 'ラーヴァナコーデックス',
+						shortName: '学本',
+						job: 0,
+						icon: 'images/item/goku_ravana/a12.png'
+					},
+					{
+						name: 'ラーヴァナプラニスフィア',
+						shortName: '天球儀',
+						job: 0,
+						icon: 'images/item/goku_ravana/a13.png'
+					}
+				]
+			},
+
+			{
 				name: '極シヴァ',
 				items: [
 					{
@@ -604,6 +696,7 @@ var data = [
 					}
 				]
 			}
+
 		]
 	}
 ];
