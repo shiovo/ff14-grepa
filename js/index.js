aebc34b2a0a423a63a3571a14c4a624d8a781366
中西汐音 <s.ide@s-arcana.co.jp>
2015-11-17 14:13:08 +0900

    極ナイツオブラウンド追加

--- js/index.js
+++ js/index.js
@@ -491,7 +491,99 @@ var data = [
 	{
 		name: '討伐・討滅戦',
 		ids: [
-				{
+			{
+				name: '極ナイツオブラウンド',
+				items: [
+					{
+						name: 'ヘヴンスソード',
+						shortName: '片手剣',
+						job: 0,
+						icon: 'images/item/goku_kor/a01.png'
+					},
+					{
+						name: 'ヘヴンスシールド',
+						shortName: '盾',
+						job: 0,
+						icon: 'images/item/goku_kor/b01.png'
+					},
+					{
+						name: 'ヘヴンスアクス',
+						shortName: '斧',
+						job: 0,
+						icon: 'images/item/goku_kor/a02.png'
+					},
+					{
+						name: 'ヘヴンスクレイモア',
+						shortName: '両手剣',
+						job: 0,
+						icon: 'images/item/goku_kor/a03.png'
+					},
+					{
+						name: 'ヘヴンスナックル',
+						shortName: '拳',
+						job: 0,
+						icon: 'images/item/goku_kor/a04.png'
+					},
+					{
+						name: 'ヘヴンスハルバード',
+						shortName: '槍',
+						job: 0,
+						icon: 'images/item/goku_kor/a06.png'
+					},
+					{
+						name: 'ヘヴンスダガー',
+						shortName: '双剣',
+						job: 0,
+						icon: 'images/item/goku_kor/a05.png'
+					},
+					{
+						name: 'ヘヴンスボウ',
+						shortName: '弓',
+						job: 0,
+						icon: 'images/item/goku_kor/a07.png'
+					},
+					{
+						name: 'ヘヴンスファイア',
+						shortName: '銃',
+						job: 0,
+						icon: 'images/item/goku_kor/a08.png'
+					},
+					{
+						name: 'ヘヴンススタッフ',
+						shortName: '黒杖',
+						job: 0,
+						icon: 'images/item/goku_kor/a09.png'
+					},
+
+					{
+						name: 'ヘヴンスグリモア',
+						shortName: '召本',
+						job: 0,
+						icon: 'images/item/goku_kor/a10.png'
+					},
+
+					{
+						name: 'ヘヴンスケーン',
+						shortName: '白杖',
+						job: 0,
+						icon: 'images/item/goku_kor/a11.png'
+					},
+					{
+						name: 'ヘヴンスコーデックス',
+						shortName: '学本',
+						job: 0,
+						icon: 'images/item/goku_kor/a12.png'
+					},
+					{
+						name: 'ヘヴンスメーター',
+						shortName: '天球儀',
+						job: 0,
+						icon: 'images/item/goku_kor/a13.png'
+					}
+				]
+			},
+
+			{
 				name: '極ラーヴァナ',
 				items: [
 					{
