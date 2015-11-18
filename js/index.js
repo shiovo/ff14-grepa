7d97ed2201a35c407d7bd0ba5622cf7ea526d656
中西汐音 <s.ide@s-arcana.co.jp>
2015-11-18 17:37:06 +0900

    アレキ零式1〜３層追加

--- js/index.js
+++ js/index.js
@@ -144,91 +144,222 @@ var data = [
 				]
 			},
 			{
+				name: 'アレキ零式2層',
+				items: [
+					{
+						name: 'ゴルディオン・ディフェンダーガントレット',
+						shortName: 'タンク手',
+						job: 0,
+						icon: 'images/item/areki_z/2/b01_1.png'
+					},
+					{
+						name: 'ゴルディオン・ディフェンダーベルト',
+						shortName: 'タンク腰',
+						job: 0,
+						icon: 'images/item/areki_z/2/b08.png'
+					},
+					{
+						name: 'ゴルディオン・ディフェンダーソルレット',
+						shortName: 'タンク足',
+						job: 0,
+						icon: 'images/item/areki_z/2/b01_3.png'
+					},
+					{
+						name: 'ゴルディオン・スレイヤーガントレット',
+						shortName: '竜手',
+						job: 0,
+						icon: 'images/item/areki_z/2/b02_1.png'
+					},
+					{
+						name: 'ゴルディオン・スレイヤーベルト',
+						shortName: '竜腰',
+						job: 0,
+						icon: 'images/item/areki_z/2/b08.png'
+					},
+					{
+						name: 'ゴルディオン・スレイヤーサバトン',
+						shortName: '竜足',
+						job: 0,
+						icon: 'images/item/areki_z/2/b02_3.png'
+					},
+					{
+						name: 'ゴルディオン・ストライカーガントレット',
+						shortName: 'モンク手',
+						job: 0,
+						icon: 'images/item/areki_z/2/b03_1.png'
+					},
+					{
+						name: 'ゴルディオン・ストライカーベルト',
+						shortName: 'モンク腰',
+						job: 0,
+						icon: 'images/item/areki_z/2/b08.png'
+					},
+					{
+						name: 'ゴルディオン・ストライカーサバトン',
+						shortName: 'モンク足',
+						job: 0,
+						icon: 'images/item/areki_z/2/b03_3.png'
+					},
+					{
+						name: 'ゴルディオン・レンジャーアームガード',
+						shortName: 'レンジ手',
+						job: 0,
+						icon: 'images/item/areki_z/2/b04_1.png'
+					},
+					{
+						name: 'ゴルディオン・レンジャーベルト',
+						shortName: 'レンジ腰',
+						job: 0,
+						icon: 'images/item/areki_z/2/b08.png'
+					},
+					{
+						name: 'ゴルディオン・レンジャーサバトン',
+						shortName: 'レンジ足',
+						job: 0,
+						icon: 'images/item/areki_z/2/b04_3.png'
+					},
+					{
+						name: 'ゴルディオン・スカウトアームガード',
+						shortName: '忍手',
+						job: 0,
+						icon: 'images/item/areki_z/2/b05_1.png'
+					},
+					{
+						name: 'ゴルディオン・スカウトベルト',
+						shortName: '忍腰',
+						job: 0,
+						icon: 'images/item/areki_z/2/b08.png'
+					},
+					{
+						name: 'ゴルディオン・スカウトサバトン',
+						shortName: '忍足',
+						job: 0,
+						icon: 'images/item/areki_z/2/b05_3.png'
+					},
+					{
+						name: 'ゴルディオン・キャスターグローブ',
+						shortName: 'キャス手',
+						job: 0,
+						icon: 'images/item/areki_z/2/b06_1.png'
+					},
+					{
+						name: 'ゴルディオン・キャスターベルト',
+						shortName: 'キャス腰',
+						job: 0,
+						icon: 'images/item/areki_z/2/b08.png'
+					},
+					{
+						name: 'ゴルディオン・キャスターガンビエラ',
+						shortName: 'キャス足',
+						job: 0,
+						icon: 'images/item/areki_z/2/b06_3.png'
+					},
+					{
+						name: 'ゴルディオン・ヒーラーグローブ',
+						shortName: 'ヒラ手',
+						job: 0,
+						icon: 'images/item/areki_z/2/b07_1.png'
+					},
+					{
+						name: 'ゴルディオン・ヒーラーベルト',
+						shortName: 'ヒラ腰',
+						job: 0,
+						icon: 'images/item/areki_z/2/b08.png'
+					},
+					{
+						name: 'ゴルディオン・ヒーラーガンビエラ',
+						shortName: 'ヒラ足',
+						job: 0,
+						icon: 'images/item/areki_z/2/b07_3.png'
+					}
+				]
+			},
+			{
 				name: 'アレキ零式3層',
 				items: [
 					{
 						name: 'ゴルディオン・ディフェンダーアーメット',
 						shortName: 'タンク頭',
 						job: 0,
-						icon: 'images/item/areki_z/2/b01_1.png'
+						icon: 'images/item/areki_z/3/b01_1.png'
 					},
 					{
 						name: 'ゴルディオン・ディフェンダーブリーチ',
 						shortName: 'タンク脚',
 						job: 0,
-						icon: 'images/item/areki_z/2/b01_2.png'
+						icon: 'images/item/areki_z/3/b01_2.png'
 					},
 					{
 						name: 'ゴルディオン・スレイヤーサレット',
 						shortName: '竜頭',
 						job: 0,
-						icon: 'images/item/areki_z/2/b02_1.png'
+						icon: 'images/item/areki_z/3/b02_1.png'
 					},
 					{
 						name: 'ゴルディオン・スレイヤーブリーチ',
 						shortName: '竜脚',
 						job: 0,
-						icon: 'images/item/areki_z/2/b02_2.png'
+						icon: 'images/item/areki_z/3/b02_2.png'
 					},
 					{
 						name: 'ゴルディオン・ストライカーサレット',
 						shortName: 'モンク頭',
 						job: 0,
-						icon: 'images/item/areki_z/2/b03_1.png'
+						icon: 'images/item/areki_z/3/b03_1.png'
 					},
 					{
 						name: 'ゴルディオン・ストライカーブリーチ',
 						shortName: 'モンク脚',
 						job: 0,
-						icon: 'images/item/areki_z/2/b03_2.png'
+						icon: 'images/item/areki_z/3/b03_2.png'
 					},
 					{
 						name: 'ゴルディオン・レンジャーフード',
 						shortName: 'レンジ頭',
 						job: 0,
-						icon: 'images/item/areki_z/2/b04_1.png'
+						icon: 'images/item/areki_z/3/b04_1.png'
 					},
 					{
 						name: 'ゴルディオン・レンジャーポレイン',
 						shortName: 'レンジ脚',
 						job: 0,
-						icon: 'images/item/areki_z/2/b04_2.png'
+						icon: 'images/item/areki_z/3/b04_2.png'
 					},
 					{
 						name: 'ゴルディオン・スカウトフード',
 						shortName: '忍頭',
 						job: 0,
-						icon: 'images/item/areki_z/2/b05_1.png'
+						icon: 'images/item/areki_z/3/b05_1.png'
 					},
 					{
 						name: 'ゴルディオン・スカウトポレイン',
 						shortName: '忍脚',
 						job: 0,
-						icon: 'images/item/areki_z/2/b05_2.png'
+						icon: 'images/item/areki_z/3/b05_2.png'
 					},
 					{
 						name: 'ゴルディオン・キャスタークラウン',
 						shortName: 'キャス頭',
 						job: 0,
-						icon: 'images/item/areki_z/2/b06_1.png'
+						icon: 'images/item/areki_z/3/b06_1.png'
 					},
 					{
 						name: 'ゴルディオン・キャスターブレエット',
 						shortName: 'キャス脚',
 						job: 0,
-						icon: 'images/item/areki_z/2/b06_2.png'
+						icon: 'images/item/areki_z/3/b06_2.png'
 					},
 					{
 						name: 'ゴルディオン・ヒーラークラウン',
 						shortName: 'ヒラ頭',
 						job: 0,
-						icon: 'images/item/areki_z/2/b07_1.png'
+						icon: 'images/item/areki_z/3/b07_1.png'
 					},
 					{
 						name: 'ゴルディオン・ヒーラーブレエット',
 						shortName: 'ヒラ脚',
 						job: 0,
-						icon: 'images/item/areki_z/2/b07_2.png'
+						icon: 'images/item/areki_z/3/b07_2.png'
 					}
 				]
 			},
