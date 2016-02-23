4bbb130b2207c50b373c85fb4fa184362a5c1384
中西汐音 <s.ide@s-arcana.co.jp>
2016-02-24 03:00:49 +0900

    極セフィロト追加

--- js/index.js
+++ js/index.js
@@ -994,6 +994,98 @@ var data = [
 		name: '討伐・討滅戦',
 		ids: [
 			{
+				name: '極セフィロト',
+				items: [
+					{
+						name: 'ブレード・オブ・セフィロト',
+						shortName: '片手剣',
+						job: 0,
+						icon: 'images/item/goku_sefi/a01.png'
+					},
+					{
+						name: 'シールド・オブ・セフィロト',
+						shortName: '盾',
+						job: 0,
+						icon: 'images/item/goku_sefi/b01.png'
+					},
+					{
+						name: 'アクス・オブ・セフィロト',
+						shortName: '斧',
+						job: 0,
+						icon: 'images/item/goku_sefi/a02.png'
+					},
+					{
+						name: 'エッジ・オブ・セフィロト',
+						shortName: '両手剣',
+						job: 0,
+						icon: 'images/item/goku_sefi/a03.png'
+					},
+					{
+						name: 'フィスト・オブ・セフィロト',
+						shortName: '拳',
+						job: 0,
+						icon: 'images/item/goku_sefi/a04.png'
+					},
+					{
+						name: 'パイク・オブ・セフィロト',
+						shortName: '槍',
+						job: 0,
+						icon: 'images/item/goku_sefi/a06.png'
+					},
+					{
+						name: 'ポインツ・オブ・セフィロト',
+						shortName: '双剣',
+						job: 0,
+						icon: 'images/item/goku_sefi/a05.png'
+					},
+					{
+						name: 'ボウ・オブ・セフィロト',
+						shortName: '弓',
+						job: 0,
+						icon: 'images/item/goku_sefi/a07.png'
+					},
+					{
+						name: 'ファイア・オブ・セフィロト',
+						shortName: '銃',
+						job: 0,
+						icon: 'images/item/goku_sefi/a08.png'
+					},
+					{
+						name: 'スタッフ・オブ・セフィロト',
+						shortName: '黒杖',
+						job: 0,
+						icon: 'images/item/goku_sefi/a09.png'
+					},
+
+					{
+						name: 'ワード・オブ・セフィロト',
+						shortName: '召本',
+						job: 0,
+						icon: 'images/item/goku_sefi/a10.png'
+					},
+
+					{
+						name: 'ケーン・オブ・セフィロト',
+						shortName: '白杖',
+						job: 0,
+						icon: 'images/item/goku_sefi/a11.png'
+					},
+					{
+						name: 'ソング・オブ・セフィロト',
+						shortName: '学本',
+						job: 0,
+						icon: 'images/item/goku_sefi/a12.png'
+					},
+					{
+						name: 'スター・オブ・セフィロト',
+						shortName: '天球儀',
+						job: 0,
+						icon: 'images/item/goku_sefi/a13.png'
+					}
+				]
+			},
+
+			{
 				name: '極ナイツ・オブ・ラウンド',
 				items: [
 					{
