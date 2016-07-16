36246f746086d31dd79a9c31815f3c28206712da
中西汐音 <s.ide@s-arcana.co.jp>
2016-07-16 15:59:43 +0900

    シヴァ

--- js-src/data/extremes/shiva.js
+++ js-src/data/extremes/shiva.js
@@ -3,54 +3,72 @@ module.exports = {
   id: 4,
   items: [
     {
+      id: 0,
+      category: 4,
       name: 'ボレアスホイッスル',
       shortName: '馬',
       job: 0,
       icon: 'images/item/goku_siva/e01.png'
     },
     {
+      id: 2,
+      category: 0,
       name: 'シヴァ・アイスブランド',
       shortName: '剣',
       job: 0,
       icon: 'images/item/goku_siva/a01.png'
     },
     {
+      id: 3,
+      category: 0,
       name: 'シヴァ・アイスアクス',
       shortName: '斧',
       job: 0,
       icon: 'images/item/goku_siva/a02.png'
     },
     {
+      id: 4,
+      category: 0,
       name: 'シヴァ・アイスシールド',
       shortName: '盾',
       job: 0,
       icon: 'images/item/goku_siva/d01.png'
     },
     {
+      id: 5,
+      category: 0,
       name: 'シヴァ・アイスクロー',
       shortName: '爪',
       job: 0,
       icon: 'images/item/goku_siva/a03.png'
     },
     {
+      id: 6,
+      category: 0,
       name: 'シヴァ・アイススピア',
       shortName: '槍',
       job: 0,
       icon: 'images/item/goku_siva/a04.png'
     },
     {
+      id: 7,
+      category: 0,
       name: 'シヴァ・アイスダガー',
       shortName: '双剣',
       job: 0,
       icon: 'images/item/goku_siva/a06.png'
     },
     {
+      id: 8,
+      category: 0,
       name: 'シヴァ・アイスボウ',
       shortName: '弓',
       job: 0,
       icon: 'images/item/goku_siva/a05.png'
     },
     {
+      id: 9,
+      category: 0,
       name: 'シヴァ・アイスロッド',
       shortName: '黒杖',
       job: 0,
@@ -58,6 +76,8 @@ module.exports = {
     },
 
     {
+      id: 10,
+      category: 0,
       name: 'シヴァ・アイスグリモア',
       shortName: '召本',
       job: 0,
@@ -65,6 +85,8 @@ module.exports = {
     },
 
     {
+      id: 11,
+      category: 0,
       name: 'シヴァ・アイスケーン',
       shortName: '白杖',
       job: 0,
@@ -72,6 +94,8 @@ module.exports = {
     },
 
     {
+      id: 12,
+      category: 0,
       name: 'シヴァ・アイスコーデックス',
       shortName: '学本',
       job: 0,
@@ -79,6 +103,8 @@ module.exports = {
     },
 
     {
+      id: 13,
+      category: 2,
       name: 'アイス・ディフェンダーブレスレット',
       shortName: 'VIT腕',
       job: 0,
@@ -86,6 +112,8 @@ module.exports = {
     },
 
     {
+      id: 14,
+      category: 2,
       name: 'アイス・アタッカーブレスレット',
       shortName: 'STR腕',
       job: 0,
@@ -93,28 +121,28 @@ module.exports = {
     },
 
     {
+      id: 15,
+      category: 2,
       name: 'アイス・レンジャーブレスレット',
       shortName: 'DEX腕',
       job: 0,
       icon: 'images/item/goku_siva/c01.png'
     },
     {
+      id: 16,
+      category: 2,
       name: 'アイス・キャスターブレスレット',
       shortName: 'INT腕',
       job: 0,
       icon: 'images/item/goku_siva/c01.png'
     },
     {
+      id: 17,
+      category: 2,
       name: 'アイス・ヒーラーブレスレット',
       shortName: 'MND腕',
       job: 0,
       icon: 'images/item/goku_siva/c01.png'
-    },
-    {
-      name: '希望品無し',
-      shortName: '無',
-      job: 0,
-      icon: 'images/item/common/none.png'
     }
   ]
 };
